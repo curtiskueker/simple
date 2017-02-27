271f2d970a3c3d8a3a992b943e14bc0efba06d02
curtiskueker <curtiskueker@gmail.com>
2017-02-27 10:36:11 -0800

    add thing class

---
+++ curtis-simple/src/main/java/org/curtis/Thing.java
@@ -0,0 +1,38 @@
+package org.curtis;
+
+/**
+ * Created by curtis on 2/27/17.
+ */
+public class Thing {
+    private Integer id;
+    private String name;
+    private String description;
+
+    public Thing() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
